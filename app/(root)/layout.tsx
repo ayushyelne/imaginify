934774daa7d35d9b5de03e1f6afdd3e82117e5ed
ayushyelne <a.yelne@traderverse.io>
2024-07-27 13:35:43 -0700

    included all components and major updates

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,18 @@
 import MobileNav from "@/components/shared/MobileNav";
 import SideBar from "@/components/shared/SideBar";
+import { Toaster } from "@/components/ui/toaster";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
-
       <SideBar />
       <MobileNav />
 
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
+      <Toaster />
     </main>
   );
 };
